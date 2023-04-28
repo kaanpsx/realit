2f6d1a05ed4fed74682c459c1de299612d0b2295
kaanpsx <131044553+kaanpsx@users.noreply.github.com>
2023-04-28 22:24:16 +0300

    Create realist.py

---
+++ realist.py
@@ -0,0 +1,7 @@
+# Hatırlamaya çalışın: hangi kütüphane listeden rastgele bir bilgi seçmenize yardımcı olabilir# Hatırlamaya çalışın: hangi kütüphane listeden rastgele bir bilgi seçmenize yardımcı olabilir?
+import random
+# Kendiniz hakkında gerçeklerin bir listesini oluşturun!
+realist = ["1 = 1", "sitem", "e=mc^2", "1 + 1 = 2", "en büyük kulaklı hayvan fildir", "√¯1 = 1"]
+# Programın rastgele gerçeği yazdırmasını sağlayın
+print(random.choice(realist))
+# Arkadaşlarınızı şaşırtın!
